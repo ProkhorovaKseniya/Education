c5c0398e65af0c56a5114aa24aff81a67dd73d5f
Kseniya Prokhorova <kseniya_pro@gmx.net>
2024-01-18 00:38:36 +0600

    relocate Lesson2

---
+++ GB/C_Sharp/Lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] array = { 2, 5, 4, 7, 8, 35, 40, 83, 38, 100 };
+int i=0;
+Console.Write($"Чётные: ");
+while (i<n) {
+    
+    if (array[i]%2==0){
+        Console.Write($"{array[i]} ");
+    }
+    i=i+1;
+}
---
+++ GB/C_Sharp/Lesson2/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿int n = 5;
+int[] array = { 2, 4, 7, 3, 1 };
+int max = array[0];
+//решение с циклом while
+int i = 0;
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.Write($"max={max}");
+
+// счётный цикл for
+max = array[0];
+for (int j=0; j<n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.Write($"max={max}");
+// цикл foreach
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.Write($"max={max}");
