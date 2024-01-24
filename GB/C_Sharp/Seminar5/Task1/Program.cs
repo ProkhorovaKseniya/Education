77944a1b6469a393f10fce051a288e6c43f5a124
Kseniya Prokhorova <kseniya_pro@gmx.net>
2024-01-25 01:20:53 +0600

    Added Seminar5 Tasks1, 2

---
+++ GB/C_Sharp/Seminar5/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿void FindReplaceToSquare(int[,] table)
+{
+    for (int i=0; i<table.GetLength(0); i++) // число столбцов
+    {
+        for (int j=0; j<table.GetLength(1); j++) // число строк
+        {
+            if (i%2==0 && j%2==0)
+            {
+                table[i,j]*=table[i,j]; // i и j четные?
+            }
+        }
+    }
+}
+void PrintTable (int[,] table)
+{
+    // foreach (int e in table)
+    // {
+    //     Console.WriteLine(e+ " ");
+
+    // }
+    for (int i=0; i<table.GetLength(0); i++) // число столбцов
+    {
+        for (int j=0; j<table.GetLength(1); j++) // число строк
+        {
+            if (i%2==0 && j%2==0)
+            {
+                Console.Write(table[i,j]+ " "); // i и j четные?
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix =
+{
+{ 1,2,3,9},
+{ 4,5,6,10},
+{ 7,8,9,11}
+};
+PrintTable(matrix);
+
+FindReplaceToSquare(matrix);
+
+PrintTable(matrix);
---
+++ GB/C_Sharp/Seminar5/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿int GetSumMainDiag(int[,] table)
+{
+    int sum=0;
+    for (int i=0; i<table.GetLength(0); i++) // число столбцов
+    {
+        for (int j=0; j<table.GetLength(1); j++) // число строк
+        {
+            if (i==j)
+            {
+                sum = sum+table[i,j];//сложение с накоплением
+            }
+        }
+    }
+    return sum;
+}
+void PrintTable (int[,] table)
+{
+    // foreach (int e in table)
+    // {
+    //     Console.WriteLine(e+ " ");
+
+    // }
+    for (int i=0; i<table.GetLength(0); i++) // число столбцов
+    {
+        for (int j=0; j<table.GetLength(1); j++) // число строк
+        {
+           
+            Console.Write(table[i,j]+ " "); // i и j четные?
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix=
+{
+{ 1,2,3,9},
+{ 4,5,6,10},
+{ 7,8,9,11}
+};
+PrintTable(matrix);
+
+int sum=GetSumMainDiag(matrix);
+
+Console.WriteLine(sum);
