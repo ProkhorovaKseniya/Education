099632a237abeae511791da5587565830fd37199
Kseniya Prokhorova <kseniya_pro@gmx.net>
2024-01-29 22:49:18 +0600

    Added Seminar6:HomeWork tasks 1-3

---
+++ GB/C_Sharp/Seminar6/Homework_Seminar6/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+void ShowStringFromAchar[] StringToCharArray(string str)
+{
+    char[] chars = new char[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    chars[i] = str[i];
+}
+return chars;
+}rray(char[] array)
+{
+    string result = "";
+    foreach (char e in array)
+    {
+        result += e;
+    }
+    Console.Write(result);
+}
+Console.Write("Введите строку:");
+string str = Console.ReadLine()!;
+char[] chars = StringToCharArray(str);
+ShowStringFromArray(chars);
+
---
+++ GB/C_Sharp/Seminar6/Homework_Seminar6/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string CheckIsPalindrom(string input)
+{
+    string str = input;
+    string palindrom = "";
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == str[input.Length - 1 - i])
+        {
+            palindrom += input[i];
+        }
+        else
+        {
+            return "No palindrom";
+        }
+    }
+
+    return palindrom + " is palindrom";
+
+}
+Console.WriteLine("Введите строку (на латинице):");
+string str = Console.ReadLine()!;
+Console.WriteLine(CheckIsPalindrom(str));
