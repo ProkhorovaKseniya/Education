b43ca5cdc58fd3ed17dec7a4a6380ec9654752c0
Kseniya Prokhorova <kseniya_pro@gmx.net>
2024-01-29 21:22:43 +0600

    Added Lesson5:Task1

---
+++ GB/C_Sharp/Lesson5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Fact(int n)
+{
+    int fact = 0;
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+     fact = n * Fact(n - 1);
+    Console.WriteLine(n+", "+fact+" ");
+    return fact;
+
+}
+Console.Write(Fact(5));
