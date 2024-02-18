6616b0075e1147d389301b12f51758c72c5107ed
Kseniya Prokhorova <kseniya_pro@gmx.net>
2024-02-18 21:21:21 +0600

    Added Seminar7 -> Task1

---
+++ GB/C_Sharp/Seminar7/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+int SumOfDigits(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    int sum = num %10 ;
+    int temp= SumOfDigits(num/10);
+    Console.WriteLine(temp);
+    sum+=temp;
+    return sum;
+}
+int res = SumOfDigits(345);
+Console.WriteLine(res);
