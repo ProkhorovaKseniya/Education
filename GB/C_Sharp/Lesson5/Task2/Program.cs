18195ff7408c45c0d3d510d30ece93ab4b2caaec
Kseniya Prokhorova <kseniya_pro@gmx.net>
2024-02-18 21:20:13 +0600

    Added Lesson5 -> Task2

---
+++ GB/C_Sharp/Lesson5/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int SumOfDigits(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    int sum = num %10 ;
+    int temp= SumOfDigits(num/10);
+    Console.WriteLine(temp);
+    sum+=temp;
+    return sum;
+}
+int res = SumOfDigits(345);
+Console.WriteLine(res);
