70877bde57a265bea302285e087a47f3b8490625
Kseniya Prokhorova <kseniya_pro@gmx.net>
2024-02-27 01:16:38 +0600

    task solved

--- GB/SummaryControlMainBlock/Task/Program.cs
+++ GB/SummaryControlMainBlock/Task/Program.cs
@@ -4,4 +4,46 @@
 //   выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 //   лучше обойтись исключительно массивами.
 
-c
+int GetQty(string[] arr, int maxLength)
+{
+    int a = 0;
+    foreach (string e in arr)
+    {
+        if (e.Length <= maxLength)
+        {
+            a++;
+        }
+    }
+    return a;
+}
+
+string[] GetShortArray(string[] arr, int maxLength)
+{
+    string[] result = new string[GetQty(arr, maxLength)];
+    int j = 0;
+    foreach (string e in arr)
+    {
+        if (e.Length <= maxLength)
+        {
+            result[j] = e;
+            //Console.Write(result[j] + ", ");
+            j++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    foreach (string e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+    Console.Write("]");
+}
+
+string[] str_arr = { "Hell0", "lor", "2", "wor", "ld", "23" };
+int maxLength = 3;
+string[] result = GetShortArray(str_arr, maxLength);
+PrintArray(result);
